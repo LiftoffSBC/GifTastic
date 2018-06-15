a32c4441a63b23a9b38fc402a0c7d61be04ee35f
LiftoffSBC <eagillet@gmail.com>
2018-06-15 01:03:37 -0400

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,102 +1,84 @@
-      // Initial array of movies
-      var cars = ["ford", "honda", "toyota", "dodge", "kia", "bmw", "audi", "gmc", "chevy", "volvo"];
+// Initial array of cars
+var cars = ["ford", "honda", "toyota", "dodge", "kia", "bmw", "audi", "gmc", "chevy", "volvo"];
 
-      // displayMovieInfo function re-renders the HTML to display the appropriate content
-      function displayCarInfo() {
+$(document).on("click", ".car", function () {
 
-        var car = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + car +  "api_key=we748bwKG6dqEfiIdJdX8nmZixYkk5x9&q=cars&limit=10&offset=0&rating=PG-13&lang=en";
-
-        // Creating an AJAX call for the specific car button being clicked
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-          console.log(response)
-          $("#cars-view").empty();
-           // console.log(queryURL)
-           for (var i = 0; i < response.data.length; i++) {
-          // Creating a div to hold the car
-          var carDiv = $("<div class='car'>");
-          // Storing the rating data
-          var rating = response.data[i].rating;
-          // Creating an element to have the rating displayed
-          var rate = $("<p>").text("Rating: " + rating);
-          // Displaying the rating
-          carDiv.append(rate);
-          // Retrieving the URL for the still image
-          var imgURL = response.data[i].images.fixed_height_still.url;
-          // Creating an element to hold the still image
-          var imageURL = $("<img>");
-          imageURL.attr("data-animate",response.data[i].images.fixed_height.url); 
-          // Appending the still image
-          carDiv.append(imgURL);
-          // Retrieving the URL for the still image
-          var imgMURL = response.data[i].images.fixed_width.url;
-          var imgMURL = $("<img>");
-          imgMURL.attr("data-still",response.data[i].images.fixed_width.url);
-          // Creating an element to hold the still image
-        //   var imageM = $("<img>").attr("src", imgMURL);
-        //   // Appending the image
-        //   carDiv.append(imageM);
-          // Putting the entire car above the previous cars
-
-           $("#cars-view").prepend(carDiv)
-          }
-    });
-    }
-        // image.attr("data-state", "still");
-        // imgMURL.attr("data-state", "animate");
-
-        $(".gif").on("click", function() {
+    var car = $(this).attr("data-name");
+    // var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=we748bwKG6dqEfiIdJdX8nmZixYkk5x9&q=cars&limit=10&offset=0&rating=PG-13&lang=en";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + car + "&api_key=we748bwKG6dqEfiIdJdX8nmZixYkk5x9&q=cars&limit=10&offset=0&rating=PG-13&lang=en";
+    // Creating an AJAX call for the specific car button being clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        $("#cars-view").empty();
+        // console.log(queryURL)
+        for (var i = 0; i < response.data.length; i++) {
+            // Creating a div to hold the car
+            var carDiv = $("<div class='car'>");
+            var rating = response.data[i].rating;
+            var rate = $("<p>").text("Rating: " + rating);
+            var img = $("<img>");
+            img.attr("src", response.data[i].images.fixed_height_still.url);
+            img.attr("data-still", response.data[i].images.fixed_height_still.url);
+            img.attr("data-animate", response.data[i].images.fixed_height.url);
+            img.attr("data-state", "still");
+            img.addClass("gif");
+            carDiv.append(img);
+            carDiv.append(rate);
+            // Putting the entire car above the previous cars
+            $("#cars-view").prepend(carDiv);
+        }
+        $(document).on("click", ".gif", function () {
             var state = $(this).attr("data-state");
 
-             if (state === "still") {
-             $(this).attr("src", $(this).attr("data-animate"));
-             $(this).attr("data-state", "animate");
-             } else {
-              $(this).attr("src", $(this).attr("data-still"));
-              $(this).attr("data-state", "still");
-             }
-      // Function for displaying car data
-      function renderButtons() {
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        });
+    });
+});
 
-        // Deleting the cars prior to adding new cars
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
+// Function for displaying car data
+function renderButtons() {
 
-        // Looping through the array of cars
-        for (var i = 0; i < cars.length; i++) {
+    // Deleting the cars prior to adding new cars
+    $("#buttons-view").empty();
 
-          // Then dynamicaly generating buttons for each car in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class of car-btn to our button
-          a.addClass("car-btn");
-          // Adding a data-attribute
-          a.attr("data-name", cars[i]);
-          // Providing the initial button text
-          a.text(cars[i]);
-          // Adding the button to the buttons-view div
-          $("#buttons-view").append(a);
-        }
-      }
+    // Looping through the array of cars
+    for (var i = 0; i < cars.length; i++) {
 
-      // This function handles events where a car button is clicked
-      $("#add-car").on("click", function(event) {
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var car = $("#car-input").val().trim();
+        // Then dynamicaly generating buttons for each car in the array
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        var a = $("<button>");
+        // Adding a class of car-btn to our button
+        a.addClass("car-btn");
 
-        // Adding car from the textbox to our array
-        cars.push(car);
-
-        // Calling renderButtons which handles the processing of our car array
-        renderButtons();
-      });
-
-      // Adding a click event listener to all elements with a class of "car-btn"
-      $(document).on("click", ".car-btn", displayCarInfo);
-
-      // Calling the renderButtons function to display the intial buttons
-      renderButtons();
+        // Adding a data-attribute
+        a.attr("data-name", cars[i]);
+        a.attr("data-state", "still");
+        // Providing the initial button text
+        a.text(cars[i]);
+        // Adding the button to the buttons-view div
+        $("#buttons-view").append(a);
+    }
+}
+// This function handles events where a car button is clicked
+$("#add-car").on("click", function (event) {
+    event.preventDefault();
+    // This line grabs the input from the textbox
+    var car = $("#car-input").val().trim();
+    // Adding car from the textbox to our array
+    cars.push(car);
+    // Calling renderButtons which handles the processing of our car array
+    renderButtons();
+});
+// Adding a click event listener to all elements with a class of "car-btn"
+// $(document).on("click", ".car", this);
+// Calling the renderButtons function to display the intial buttons
+renderButtons();
